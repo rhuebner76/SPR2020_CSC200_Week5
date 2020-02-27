23deeb6e81f811fd261c900f3e716e2b07af05ca
Russell Huebner <russell.j.huebner@outlook.com>
2020-02-27 10:20:38 -0800

    Added SayHi SayGoodbye

--- Week5ConsoleApp/Program.cs
+++ Week5ConsoleApp/Program.cs
@@ -10,6 +10,18 @@ namespace Week5ConsoleApp
     {
         static void Main(string[] args)
         {
+            SayHello();
+            SayGoodbye();
+        }
+
+        public static void SayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        public static void SayGoodbye()
+        {
+            Console.WriteLine("Goodbye");
         }
     }
 }
